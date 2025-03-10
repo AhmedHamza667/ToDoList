3c2ac27e9950611b3450fa6b95d88104ed9af164
AhmedHamza667 <hamzaa667788@gmail.com>
2025-03-10 13:15:13 -0400

    Initial Commit

---
+++ ToDoListCoreData/ToDoListCoreDataApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoListCoreDataApp.swift
+//  ToDoListCoreData
+//
+//  Created by Ahmed Hamza on 3/10/25.
+//
+
+import SwiftUI
+
+@main
+struct ToDoListCoreDataApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
